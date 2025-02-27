f3e0275ac1bc5aaa6d016a1494a790b5e928f05f
Athul Babu <athul.6560@gmail.com>
2025-02-27 13:42:35 -0600

    register

--- app/src/main/java/com/zeezaglobal/digitalprescription/Activities/MainActivity.kt
+++ app/src/main/java/com/zeezaglobal/digitalprescription/Activities/MainActivity.kt
@@ -19,12 +19,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val nextButton: AppCompatButton = findViewById(R.id.button)
+        val loginBtn: AppCompatButton = findViewById(R.id.login_btn)
+        val registerBtn: AppCompatButton = findViewById(R.id.register_btn)
 
         // Set an onClickListener on the "Next" button
-        nextButton.setOnClickListener {
+        loginBtn.setOnClickListener {
             // Create an intent to navigate to the new activity
             val intent = Intent(this, IntroActivity::class.java)
+            startActivity(intent)
+        }
+        registerBtn.setOnClickListener {
+            // Create an intent to navigate to the new activity
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
