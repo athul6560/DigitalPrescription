f6526ab36b7a3fe70828fb89904a647e33aef322
Athul Babu <athul.6560@gmail.com>
2025-03-13 16:38:50 -0500

    added ppayment design

---
+++ app/src/main/java/com/zeezaglobal/digitalprescription/Dialoge/BottomSheetDialog.kt
@@ -0,0 +1,34 @@
+package com.zeezaglobal.digitalprescription.Dialoge
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.zeezaglobal.digitalprescription.R
+
+class BottomSheetDialog : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the bottom sheet layout
+        val view = inflater.inflate(R.layout.bottom_sheet_layout, container, false)
+
+        // Find buttons from the layout
+        val algoButton: Button = view.findViewById(R.id.button_1)
+
+
+        // Set click listener for Algorithm button
+        algoButton.setOnClickListener {
+            Toast.makeText(activity, "First Button Clicked", Toast.LENGTH_SHORT).show()
+            dismiss() // Close the bottom sheet
+        }
+
+
+
+        return view
+    }
+}
